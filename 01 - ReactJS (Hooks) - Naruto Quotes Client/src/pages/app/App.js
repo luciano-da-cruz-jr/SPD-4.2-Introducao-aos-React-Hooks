068944f2a915a271b497d43d91f1a991d47d313c
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-23 21:13:26 -0300

    ReactJS (Hooks) - Naruto Quotes Client V3 - Conhecendo o React Styled Components

--- 01 - ReactJS (Hooks) - Naruto Quotes Client/src/pages/app/App.js
+++ 01 - ReactJS (Hooks) - Naruto Quotes Client/src/pages/app/App.js
@@ -1,13 +1,25 @@
+import styled from 'styled-components';
 import narutoImg from '../../images/naruto.png'
 
 
 export function App() 
 {
   return (
-    <div>
-      <p>quote xpto - Speaker</p>
-      <button>Quote No Jutsu</button>
+    <Content>
       <img src={narutoImg} alt="Naruto with a kunai"/>
-    </div>
+    </Content>
   );
 }
+
+const Content = styled.div`
+  height: 100vh;
+  padding: 0 50px;
+  display: flex;
+  justify-content: center
+  align-itens: center;
+`;
+
+const narutoImg = styled.img`
+  max-width: 50vw;
+  align-self: flex-end;
+`;
