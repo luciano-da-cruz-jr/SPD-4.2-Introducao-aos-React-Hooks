7a4d35c0cb942e4af3d81bc3cfd69d7dca0f7ac8
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-24 07:32:05 -0300

    ReactJS (Hooks) - Naruto Quotes Client V8 - Evoluindo nosso projeto através do controle de estado

--- 01 - ReactJS (Hooks) - Naruto Quotes Client/src/pages/app/App.test.js
+++ 01 - ReactJS (Hooks) - Naruto Quotes Client/src/pages/app/App.test.js
@@ -1,5 +1,19 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { App } from './App';
+
+const response = { speaker: 'Speaker', quote: 'test quote'};
+
+const server = setupServer(
+  rest.get(process.env.REACT_APP_API, (req, res, ctx) => {
+    return res(ctx.json(response));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('renders the app with a button, a quote and a button', () => 
 {
@@ -12,4 +26,17 @@ test('renders the app with a button, a quote and a button', () =>
   expect(buttonEl).toBeInTheDocument();
   expect(imageEl).toBeInTheDocument();
   expect(textEl).toBeInTheDocument();
+});
+
+test('calls api on button click and update its text', async () => 
+{
+  render(<App />);
+
+  const buttonEl = screen.getByRole('button');
+
+  fireEvent.click(buttonEl);
+
+  const quoteEl = await screen.findByText(response.quote);
+
+  expect(quoteEl).toBeInTheDocument();
 });
